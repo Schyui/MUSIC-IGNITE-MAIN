5b5b2f15c5751ac61c3788dfac5ee30ef47ce72b
Wilmar <opina.346025@dasmarinas.sti.edu.ph>
2025-03-30 16:39:25 +0800

    Activities from Beginner to Advanced

--- app/src/main/java/com/example/musicignite/Advanced_path.java
+++ app/src/main/java/com/example/musicignite/Advanced_path.java
@@ -2,7 +2,6 @@ package com.example.musicignite;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,23 +10,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Learn_Activity extends AppCompatActivity {
+public class Advanced_path extends AppCompatActivity {
 
     ImageView backBtn;
-    Button beginnerBtn, intermediateBtn, advancedBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_learn);
+        setContentView(R.layout.activity_advanced_path);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        beginnerBtn = findViewById(R.id.beginnerBtn);
-        intermediateBtn = findViewById(R.id.intermediateBtn);
-        advancedBtn = findViewById(R.id.advancedBtn);
         backBtn = findViewById(R.id.backBtn);
     }
 
@@ -35,25 +30,8 @@ public class Learn_Activity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         backBtn.setOnClickListener(view -> {
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-
-        });
-        beginnerBtn.setOnClickListener(view -> {
-            Intent intent = new Intent(this, Beginner_path.class);
-            startActivity(intent);
-        });
-
-        intermediateBtn.setOnClickListener(view -> {
-            Intent intent = new Intent(this, Intermediate_path.class);
-            startActivity(intent);
-        });
-
-        advancedBtn.setOnClickListener(view -> {
-            Intent intent = new Intent(this, Advanced_path.class);
+            Intent intent = new Intent(this, Learn_Activity.class);
             startActivity(intent);
         });
     }
-
-
 }
