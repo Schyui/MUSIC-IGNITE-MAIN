55fb2bd90db3236601145720ccfc62c009c66212
Wilmar <opina.346025@dasmarinas.sti.edu.ph>
2025-04-29 00:36:05 +0800

    sa btn border may pinalitan ako na effects pag onclick, pede nyo sya ibahin basta btn border ang nilagyan ng ripple at btn custom ang gamitin kung custom fade ang gawin

---
+++ app/src/main/java/com/example/musicignite/Learn_Basics_L1.java
@@ -0,0 +1,36 @@
+package com.example.musicignite;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Learn_Basics_L1 extends AppCompatActivity {
+
+    ImageView backBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_learn_basics_l1);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        backBtn = findViewById(R.id.backBtn);
+    }
+@Override
+    public void onStart() {
+        super.onStart();
+        backBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(this, Learn_Basics.class);
+        });
+
+    }
+}
