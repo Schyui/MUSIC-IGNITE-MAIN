20b3b1288122d8c64317ce753140a4031e90b4cd
Harbi <harveymellomida123@gmail.com>
2025-05-24 21:36:02 +0800

    TERMS AND CONDITION WITH PREVIOUS BUTTON AND SCROLLABLE

--- app/src/main/java/com/example/musicignite/SettingsFragment.java
+++ app/src/main/java/com/example/musicignite/SettingsFragment.java
@@ -9,11 +9,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 
 public class SettingsFragment extends Fragment {
 
-    TextView logout;
+    TextView logout, terms;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -22,12 +24,24 @@ public class SettingsFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
 
         logout = view.findViewById(R.id.logoutBtn);
+        terms = view.findViewById(R.id.terms);
         logout.setOnClickListener(v -> {
             Toast.makeText(requireContext(), "Logout!", Toast.LENGTH_SHORT).show();
             Intent intent = new Intent(requireContext(), LoginActivity.class);
             startActivity(intent);
         });
+
         return view;
+
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        terms.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), terms.class);
+            startActivity(intent);
+        });
+
+    }
 }
