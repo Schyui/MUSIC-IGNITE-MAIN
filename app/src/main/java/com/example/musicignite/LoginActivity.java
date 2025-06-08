f513525d354b9826c5a25e2e0f095f0232ace526
hajiireee <javier.329671@dasmarinas.sti.edu.ph>
2025-06-08 11:57:43 +0800

    hide and unhide password sa login and signup, confirm pass in sign up.

--- app/src/main/java/com/example/musicignite/LoginActivity.java
+++ app/src/main/java/com/example/musicignite/LoginActivity.java
@@ -2,10 +2,13 @@ package com.example.musicignite;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,7 +31,8 @@ public class LoginActivity extends AppCompatActivity {
     EditText loginUsername, loginPassword;
     Button loginButton;
     TextView signupRedirectText;
-
+    ImageView passwordToggle;
+    boolean isPasswordVisible = false;
     String usernameTxt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +48,20 @@ public class LoginActivity extends AppCompatActivity {
         loginPassword = findViewById(R.id.login_password);
         loginButton = findViewById(R.id.login_button);
         signupRedirectText = findViewById(R.id.signupRedirectText);
+        passwordToggle = findViewById(R.id.password_toggle);
+
+        passwordToggle.setOnClickListener(view -> {
+            if (isPasswordVisible) {
+                loginPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                passwordToggle.setImageResource(R.drawable.unhide);
+            } else {
+                loginPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                passwordToggle.setImageResource(R.drawable.hide);
+            }
+            loginPassword.setTypeface(Typeface.DEFAULT);
+            loginPassword.setSelection(loginPassword.getText().length());
+            isPasswordVisible = !isPasswordVisible;
+        });
 
         SharedPreferences prefs = getSharedPreferences("MyPrefs", MODE_PRIVATE);
         boolean isLoggedIn = prefs.getBoolean("is_logged_in", true);
--- app/src/main/java/com/example/musicignite/SignUpActivity.java
+++ app/src/main/java/com/example/musicignite/SignUpActivity.java
@@ -1,11 +1,13 @@
 package com.example.musicignite;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,9 +22,13 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpActivity extends AppCompatActivity {
 
-    EditText signupName, signupUsername, signupEmail, signupPassword;
+    EditText signupName, signupUsername, signupEmail, signupPassword, signupConfirmPassword;
     TextView loginRedirectText;
     Button signupButton;
+    ImageView passwordToggle, confirmPasswordToggle;
+    boolean isPasswordVisible = false;
+    boolean isConfirmPasswordVisible = false;
+
     FirebaseDatabase database;
     DatabaseReference reference;
 
@@ -31,63 +37,100 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_up);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
         signupName = findViewById(R.id.signup_name);
         signupEmail = findViewById(R.id.signup_email);
         signupUsername = findViewById(R.id.signup_username);
         signupPassword = findViewById(R.id.signup_password);
+        signupConfirmPassword = findViewById(R.id.signup_confirm_password);
         loginRedirectText = findViewById(R.id.loginRedirectText);
         signupButton = findViewById(R.id.signup_button);
+        passwordToggle = findViewById(R.id.password_toggle);
+        confirmPasswordToggle = findViewById(R.id.confirm_password_toggle);
 
-        signupButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                database = FirebaseDatabase.getInstance();
-                reference = database.getReference("users");
-                String name = signupName.getText().toString();
-                String email = signupEmail.getText().toString();
-                String username = signupUsername.getText().toString();
-                String password = signupPassword.getText().toString();
-
-                if (name.isEmpty() || email.isEmpty() || username.isEmpty() || password.isEmpty()) {
-                    Toast.makeText(SignUpActivity.this, "Please fill in all fields!", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if (name.length() > 15) {
-                    Toast.makeText(SignUpActivity.this, "Name cannot exceed 25 characters!", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if (username.length() > 15) {
-                    Toast.makeText(SignUpActivity.this, "Username cannot exceed 25 characters!", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                String emailPattern = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
-                if (!email.matches(emailPattern)) {
-                    Toast.makeText(SignUpActivity.this, "\"Invalid email! Format should be: example@domain.com", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-
-
-                HelperClass helperClass = new HelperClass(name, email, username, password);
-                reference.child(username).setValue(helperClass);
-                Toast.makeText(SignUpActivity.this, "You have signup successfully!", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
-                startActivity(intent);
+        passwordToggle.setOnClickListener(view -> {
+            if (isPasswordVisible) {
+                signupPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                passwordToggle.setImageResource(R.drawable.unhide);
+            } else {
+                signupPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                passwordToggle.setImageResource(R.drawable.hide);
             }
+            signupPassword.setTypeface(Typeface.DEFAULT);
+            signupPassword.setSelection(signupPassword.getText().length());
+            isPasswordVisible = !isPasswordVisible;
         });
-        loginRedirectText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
-                startActivity(intent);
+        confirmPasswordToggle.setOnClickListener(view -> {
+            if (isConfirmPasswordVisible) {
+                signupConfirmPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                confirmPasswordToggle.setImageResource(R.drawable.unhide);
+            } else {
+                signupConfirmPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                confirmPasswordToggle.setImageResource(R.drawable.hide);
             }
+            signupConfirmPassword.setTypeface(Typeface.DEFAULT);
+            signupConfirmPassword.setSelection(signupConfirmPassword.getText().length());
+            isConfirmPasswordVisible = !isConfirmPasswordVisible;
+        });
+
+        signupButton.setOnClickListener(view -> {
+            database = FirebaseDatabase.getInstance();
+            reference = database.getReference("users");
+
+            String name = signupName.getText().toString().trim();
+            String email = signupEmail.getText().toString().trim();
+            String username = signupUsername.getText().toString().trim();
+            String password = signupPassword.getText().toString().trim();
+            String confirmPassword = signupConfirmPassword.getText().toString().trim();
+
+            if (name.isEmpty() || email.isEmpty() || username.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+                Toast.makeText(SignUpActivity.this, "Please fill in all fields!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if (name.length() > 25) {
+                Toast.makeText(SignUpActivity.this, "Name cannot exceed 25 characters!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if (username.length() > 15) {
+                Toast.makeText(SignUpActivity.this, "Username cannot exceed 15 characters!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            String emailPattern = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+            if (!email.matches(emailPattern)) {
+                Toast.makeText(SignUpActivity.this, "Invalid email! Format: example@domain.com", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if (password.length() < 6) {
+                Toast.makeText(SignUpActivity.this, "Password must be at least 6 characters!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if (!password.equals(confirmPassword)) {
+                Toast.makeText(SignUpActivity.this, "Passwords do not match!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            HelperClass helperClass = new HelperClass(name, email, username, password);
+            reference.child(username).setValue(helperClass);
+            Toast.makeText(SignUpActivity.this, "You have signed up successfully!", Toast.LENGTH_SHORT).show();
+
+            startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
+            finish();
+        });
+
+        loginRedirectText.setOnClickListener(view -> {
+            startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
         });
     }
 }
